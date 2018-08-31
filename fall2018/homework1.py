bba66e0ecdf0986c8ca59759443c61e167ced9b5
Sumin Byeon <suminb@gmail.com>
2018-08-31 22:57:04 +0900

    Dynamically import solution implementation

--- fall2018/homework1.py
+++
@@ -1,2 +0,0 @@
-def convert(text):
-    raise NotImplementedError
--- fall2018/test_homework1.py
+++ fall2018/test_homework1.py
@@ -1,9 +1,41 @@
-from homework1 import convert
+import math
+import os
+import random
+import re
+
+import pytest
 
 
-def test_problem1():
+random.seed(0)
+solution = None
+
+
+def setup_module():
+    solution_files = [fn for fn in os.listdir() if re.match(r'solution1_\w+\.py', fn)]
+    assert len(solution_files) > 0, 'Could not find any solution'
+
+    module_name = solution_files[0][:-3]
+
+    global solution
+    solution = __import__(module_name)
+
+
+@pytest.mark.parametrize('i', range(10))
+def test_problem1_1(i):
+    xs = random.sample(range(100), random.randint(5, 25))
+    # NOTE: We decided to call `sqrt()` so that we don't leak the solution for this problem
+    assert all([float(x) == math.sqrt(y) for x, y in zip(xs, solution.square(xs))])
+
+
+@pytest.mark.parametrize('i', range(10))
+def test_problem1_2(i):
+    xs = random.sample(range(100), random.randint(5, 25))
+    assert all([x % 2 == 0 for x in solution.even(xs)])
+
+
+def test_problem2():
     text = 'alejandro, britney, christina, dennis, emily'
     expected = \
         'alejandro@gmail.com; britney@gmail.com; christina@gmail.com; ' \
         'dennis@gmail.com; emily@gmail.com'
-    assert convert(text) == expected
+    assert solution.convert(text) == expected
